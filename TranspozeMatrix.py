435dcd2cde6469df8c264cb2e733c13b97c6439c
Yunus Emre Özgel <75580560+IamYunusEmreOzgel@users.noreply.github.com>
2021-05-05 19:41:00 +0300

    Update TranspozeMatrix.py

--- TranspozeMatrix.py
+++ TranspozeMatrix.py
@@ -1,22 +1,28 @@
+def takeTranspose(Rows,Columns):
+    global transpozeMatrix
+    for a in range(Rows):   
+        for b in range(Columns):
+            transpozeMatrix[b][a] = Matrix[a][b]
+        
+def printMatrix(matrix):
+    for a in matrix:
+        print(a)
+
+
 Rows = int(input("How many rows would you like to have? : " ))
 Columns = int(input("How many Columns would you like to have?: "))
 
-
-
 Matrix= [[int(input()) for i in range(Columns)] for i in range(Rows)]
-
-print("Start entering the elements:")
-
-print("The Matrix Given By User")
-for a in Matrix:
-    print(a)
-
 transpozeMatrix =[[0 for i in range(Rows)] for j in range(Columns)]
 
-for r in range(Rows):   
-   for c in range(Columns):
-       transpozeMatrix[c][r] = Matrix[r][c]
+print("Start entering the matrixs elements:")
+
+
+
+print("The Matrix Given By User")
+printMatrix(Matrix)
+
+takeTranspose(Rows, Columns)
 
 print("Transpose Of The Matrix: ")
-for b in transpozeMatrix:
-    print(b)
+printMatrix(transpozeMatrix)
